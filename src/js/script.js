0e499dfb4bd2e71c19f70ab2358794e345e381c1
Russs11 <russs.khalilov@gmail.com>
2022-05-12 14:30:50 +0300

    w12.05.2022

--- src/js/script.js
+++ src/js/script.js
@@ -131,156 +131,131 @@
 // console.log('в градусах цельсия - ', tfToTc(temp));
 
 //задача №3
-let inputNumber = 93
-let str = ""
-let strDec = ""
-let strUnit = ""
-let strTwo = ""
-if (inputNumber > 99) {
-    let res = inputNumber / 100
-    // console.log("количество сотен ", res)
-    if (res >= 1) {
-        str = str + "сто "
-        if (res >= 2) {
-            str = "двести "
-        }
-        if (res >= 3) {
-            str = "триста "
-        }
-        if (res >= 4) {
-            str = "четыреста "
-        }
-        if (res >= 5) {
-            str = "пятьсот "
-        }
-        if (res >= 6) {
-            str = "шестьсот "
-        }
-        if (res >= 7) {
-            str = "семьсот "
-        }
-        if (res >= 8) {
-            str = "восемьсот "
-        }
-        if (res >= 9) {
-            str = "девятьсот "
-        }
-    }
+let array = [325, 12, 88, 9]
+array.push(55)
 
-    let resDec = inputNumber % 100
-    if (resDec >= 10) {
-        strDec = "десять "
+function numbersToWords(x) {
+    let inputNumber = x
+    let str = ""
+    let strDec = ""
+    let strUnit = ""
+    let strTwo = ""
+    let strTwoUnit = ""
+    if (inputNumber > 99) {
+        let res = inputNumber / 100
+        // console.log("количество сотен ", res)
+        if (res >= 1) {
+            str = str + "сто "
+            if (res >= 2) {
+                str = "двести "
+            }
+            if (res >= 3) {
+                str = "триста "
+            }
+            if (res >= 4) {
+                str = "четыреста "
+            }
+            if (res >= 5) {
+                str = "пятьсот "
+            }
+            if (res >= 6) {
+                str = "шестьсот "
+            }
+            if (res >= 7) {
+                str = "семьсот "
+            }
+            if (res >= 8) {
+                str = "восемьсот "
+            }
+            if (res >= 9) {
+                str = "девятьсот "
+            }
+        }
 
-        if (resDec >= 20) {
-            strDec = "двадцать "
-        }
-        if (resDec >= 30) {
-            strDec = "тридцать "
-        }
-        if (resDec >= 40) {
-            strDec = "сорок "
-        }
-        if (resDec >= 50) {
-            strDec = "пятьдесят "
-        }
-        if (resDec >= 60) {
-            strDec = "шестьдесят "
-        }
-        if (resDec >= 70) {
-            strDec = "семьдесят "
-        }
-        if (resDec >= 80) {
-            strDec = "восемьдесят "
-        }
-        if (resDec >= 90) {
-            strDec = "девяносто "
-        }
-        if (resDec === 11) {
-            strDec = "одиннадцать "
-        }
-        if (resDec === 12) {
-            strDec = "двенадцать "
-        }
-        if (resDec === 13) {
-            strDec = "тринадцать "
-        }
-        if (resDec === 14) {
-            strDec = "четырнадцать "
-        }
-        if (resDec === 15) {
-            strDec = "пятнадцать "
-        }
-        if (resDec === 16) {
-            strDec = "шестнадцать "
-        }
-        if (resDec === 17) {
-            strDec = "семнадцать "
-        }
-        if (resDec === 18) {
-            strDec = "восемнадцать "
-        }
-        if (resDec === 19) {
-            strDec = "девятнадцать "
-        }
-    }
+        let resDec = inputNumber % 100
+        if (resDec >= 10) {
+            strDec = "десять "
 
-    if (resDec > 10 && resDec < 20) {
-    }
-    else {
-        let resUnit = inputNumber % 10
-        if (resUnit === 0) {
-            strUnit = ""
-        } if (resUnit === 1) {
-            strUnit = "один "
-        } if (resUnit === 2) {
-            strUnit = "два "
-        } if (resUnit === 3) {
-            strUnit = "три "
-        } if (resUnit === 4) {
-            strUnit = "четыре "
-        } if (resUnit === 5) {
-            strUnit = "пять "
-        }if (resUnit === 6) {
-            strUnit = "шесть "
-        } if (resUnit === 7) {
-            strUnit = "семь "
-        } if (resUnit === 8) {
-            strUnit = "восемь "
-        } if (resUnit === 9) {
-            strUnit = "девять "
+            if (resDec >= 20) {
+                strDec = "двадцать "
+            }
+            if (resDec >= 30) {
+                strDec = "тридцать "
+            }
+            if (resDec >= 40) {
+                strDec = "сорок "
+            }
+            if (resDec >= 50) {
+                strDec = "пятьдесят "
+            }
+            if (resDec >= 60) {
+                strDec = "шестьдесят "
+            }
+            if (resDec >= 70) {
+                strDec = "семьдесят "
+            }
+            if (resDec >= 80) {
+                strDec = "восемьдесят "
+            }
+            if (resDec >= 90) {
+                strDec = "девяносто "
+            }
+            if (resDec === 11) {
+                strDec = "одиннадцать "
+            }
+            if (resDec === 12) {
+                strDec = "двенадцать "
+            }
+            if (resDec === 13) {
+                strDec = "тринадцать "
+            }
+            if (resDec === 14) {
+                strDec = "четырнадцать "
+            }
+            if (resDec === 15) {
+                strDec = "пятнадцать "
+            }
+            if (resDec === 16) {
+                strDec = "шестнадцать "
+            }
+            if (resDec === 17) {
+                strDec = "семнадцать "
+            }
+            if (resDec === 18) {
+                strDec = "восемнадцать "
+            }
+            if (resDec === 19) {
+                strDec = "девятнадцать "
+            }
         }
-    }
 
-    if (inputNumber < 100 && inputNumber > 9) {
-        let resTwo = inputNumber / 10
-        if (resTwo >= 1) {
-            strTwo = "десять "
-            if (resTwo >= 2) {
-                strTwo = "двадцать "
-            }
-            if (resTwo >= 3) {
-                strTwo = "тридцать "
-            }
-            if (resTwo >= 4) {
-                strTwo = "сорок "
-            }
-            if (resTwo >= 5) {
-                strTwo = "пятьдесят "
-            }
-            if (resTwo >= 6) {
-                strTwo = "шестьдесят "
-            }
-            if (resTwo >= 7) {
-                strTwo = "семьдесят "
-            }
-            if (resTwo >= 8) {
-                strTwo = "восемьдесят "
-            }
-            if (resTwo >= 9) {
-                strTwo = "девяносто "
+        if (resDec > 10 && resDec < 20) {
+        }
+        else {
+            let resUnit = inputNumber % 10
+            if (resUnit === 0) {
+                strUnit = ""
+            } if (resUnit === 1) {
+                strUnit = "один "
+            } if (resUnit === 2) {
+                strUnit = "два "
+            } if (resUnit === 3) {
+                strUnit = "три "
+            } if (resUnit === 4) {
+                strUnit = "четыре "
+            } if (resUnit === 5) {
+                strUnit = "пять "
+            } if (resUnit === 6) {
+                strUnit = "шесть "
+            } if (resUnit === 7) {
+                strUnit = "семь "
+            } if (resUnit === 8) {
+                strUnit = "восемь "
+            } if (resUnit === 9) {
+                strUnit = "девять "
             }
         }
-    }
+
 
         // let resDec = inputNumber % 100
         // if (resDec >= 10) {
@@ -310,5 +285,125 @@ if (inputNumber > 99) {
         //     if (resDec >= 90) {
         //         strDec = "девяносто "
         //     }
+    }
+    if (inputNumber < 100 && inputNumber > 9) {
+        // console.log("двузначные числа ")
+        let resTwo = inputNumber / 10
+        if (inputNumber > 9 && inputNumber < 20) {
+            if (inputNumber === 10) {
+                strDec = "десять "
+            }
+            if (inputNumber === 11) {
+                strDec = "одиннадцать "
+            }
+            if (inputNumber === 12) {
+                strDec = "двенадцать "
+            }
+            if (inputNumber === 13) {
+                strDec = "тринадцать "
+            }
+            if (inputNumber === 14) {
+                strDec = "четырнадцать "
+            }
+            if (inputNumber === 15) {
+                strDec = "пятнадцать "
+            }
+            if (inputNumber === 16) {
+                strDec = "шестнадцать "
+            }
+            if (inputNumber === 17) {
+                strDec = "семнадцать "
+            }
+            if (inputNumber === 18) {
+                strDec = "восемнадцать "
+            }
+            if (inputNumber === 19) {
+                strDec = "девятнадцать "
+            }
+
+        } else {
+            if (resTwo >= 1) {
+                strTwo = "десять "
+                if (resTwo >= 2) {
+                    strTwo = "двадцать "
+                }
+                if (resTwo >= 3) {
+                    strTwo = "тридцать "
+                }
+                if (resTwo >= 4) {
+                    strTwo = "сорок "
+                }
+                if (resTwo >= 5) {
+                    strTwo = "пятьдесят "
+                }
+                if (resTwo >= 6) {
+                    strTwo = "шестьдесят "
+                }
+                if (resTwo >= 7) {
+                    strTwo = "семьдесят "
+                }
+                if (resTwo >= 8) {
+                    strTwo = "восемьдесят "
+                }
+                if (resTwo >= 9) {
+                    strTwo = "девяносто "
+                }
+            }
+            let resUnit = inputNumber % 10
+            if (resUnit === 0) {
+                strTwoUnit = ""
+            } if (resUnit === 1) {
+                strTwoUnit = "один "
+            } if (resUnit === 2) {
+                strTwoUnit = "два "
+            } if (resUnit === 3) {
+                strTwoUnit = "три "
+            } if (resUnit === 4) {
+                strTwoUnit = "четыре "
+            } if (resUnit === 5) {
+                strTwoUnit = "пять "
+            } if (resUnit === 6) {
+                strTwoUnit = "шесть "
+            } if (resUnit === 7) {
+                strTwoUnit = "семь "
+            } if (resUnit === 8) {
+                strTwoUnit = "восемь "
+            } if (resUnit === 9) {
+                strTwoUnit = "девять "
+            }
+        }
+
+    }
+    if (inputNumber < 10) {
+        if (inputNumber === 0) {
+            strUnit = "ноль "
+        } if (inputNumber === 1) {
+            strUnit = "один "
+        } if (inputNumber === 2) {
+            strUnit = "два "
+        } if (inputNumber === 3) {
+            strUnit = "три "
+        } if (inputNumber === 4) {
+            strUnit = "четыре "
+        } if (inputNumber === 5) {
+            strUnit = "пять "
+        } if (inputNumber === 6) {
+            strUnit = "шесть "
+        } if (inputNumber === 7) {
+            strUnit = "семь "
+        } if (inputNumber === 8) {
+            strUnit = "восемь "
+        } if (inputNumber === 9) {
+            strUnit = "девять "
+        }
+    }
+    return str + strDec + strUnit + strTwo + strTwoUnit
 }
-console.log(str + strDec + strUnit + strTwo);
+
+for (let i = 0; i < array.length; i++) {
+    let v = numbersToWords(array[i])
+
+    console.log(v);
+}
+
+
