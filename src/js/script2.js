eeac77092f1b3620c960cad82e8974d4854b44fb
Russs11 <russs.khalilov@gmail.com>
2022-06-06 14:34:57 +0300

    w06.06.2022

--- src/js/script2.js
+++ src/js/script2.js
@@ -42,13 +42,13 @@ for (let i = 0; i < 10; i++) {
     fruct.setType(o)
     fructArray.push(fruct)
 }
-console.log(fructArray);
+// console.log(fructArray);
 for (let i = 0; i < 10; i++) {
     const fruct = new Fruct(Math.round(Math.random() * (100 - 90) + 90), Math.round(Math.random() * (100 - 90) + 90), count++)
     fruct.setType(g)
     fructArray.push(fruct)
 }
-console.log(fructArray);
+// console.log(fructArray);
 
 function Neighbors(x1, y1, x2, y2) {
     const distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -73,12 +73,58 @@ function whatFruct(unkcnownFruct, Fructs) {
         settIndex(index) {
             this.index = index
         }
-
     }
+    //-------------------------
+
     const distancesArray = []
-    for (let i = 0; i < 20; i++) {
+    let oCounter = 0
+    let gCounter = 0
+    //-------------------------
+    for (let i = 0; i < fructArray.length; i++) {
         distancesArray.push(new DistObj(Math.round(Neighbors(unkcnownFruct.getColor(), unkcnownFruct.getSize(), Fructs[i].getColor(), Fructs[i].getSize())), Fructs[i].getNumber()))
     }
-    console.log(distancesArray);
+    // console.log("до сортировки ", distancesArray);
+    const distancesArray2 = [...distancesArray]
+    distancesArray2.sort(function (a, b) {
+        if (a.getDist() < b.getDist()) {
+            return -1
+        } if (a.getDist() > b.getDist()) {
+            return 1
+        }
+        return 0
+    })
+
+    // console.log("после сортировки ", distancesArray2);
+    for (let i = 0; i < 5; i++) {
+        // distancesArray2.getindex()
+        let fructIndex = distancesArray2[i].getIndex()
+        for (let j = 0; j < fructArray.length; j++) {
+            if (fructArray[j].getNumber() === fructIndex) {
+                if (fructArray[j].getType() === o) {
+                    // oCounter = oCounter + 1
+                    // oCounter += 1
+                    oCounter++
+                }
+                if (fructArray[j].getType() === g) {
+                    // oCounter = oCounter + 1
+                    // oCounter += 1
+                    gCounter++
+                }
+            }
+        }
+    }
+    // console.log(oCounter, gCounter);
+    if (oCounter > gCounter) {
+        return o
+    }
+    if (oCounter < gCounter) {
+        return g
+    }
 }
-whatFruct(new Fruct(Math.round(Math.random() * (100 - 90) + 90), Math.round(Math.random() * (100 - 90) + 90)), fructArray)
+for (let i = 0; i < 5000; i++) {
+    const fruct = new Fruct(Math.round(Math.random() * 100), Math.round(Math.random() * 100), fructArray.length)
+    fruct.setType(whatFruct(fruct, fructArray))
+    fructArray.push(fruct)
+}
+console.log(fructArray);
+// whatFruct(new Fruct(Math.round(Math.random() * (100 - 90) + 90), Math.round(Math.random() * (100 - 90) + 90)), fructArray)
