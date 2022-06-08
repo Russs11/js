0f3434b4e2857a32f6177740333738ff5eb01e4c
Russs <russs.khalilov@gmail.com>
2022-06-08 23:24:09 +0300

    add_stearingWheel_08.06.2022

--- src/js/script3.js
+++ src/js/script3.js
@@ -1,11 +1,17 @@
 
-function squareItUp(startingNumber) {
-    square = startingNumber * startingNumber;
-    if (square > 1000000) {
-    console.log(square);
-    } else {
-        squareItUp(square);
-    }
-}
-squareItUp(333)
+// function squareItUp(startingNumber) {
+//     square = startingNumber * startingNumber;
+//     if (square > 1000000) {
+//     console.log(square);
+//     } else {
+//         squareItUp(square);
+//     }
+// }
+// squareItUp(333)
 
+function multiplication(a,b) {
+
+    s = a * b;
+    console.log(s);
+}
+multiplication(78,38)
