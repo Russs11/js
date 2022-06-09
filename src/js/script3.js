bc36393b2a3ee52ed63e4b2c53e8e4f480c905d7
Russs <russs.khalilov@gmail.com>
2022-06-09 22:28:09 +0300

    add_task_3_09.06.2022

--- src/js/script3.js
+++ src/js/script3.js
@@ -20,30 +20,43 @@
 // multiplication(78, 38)
 
 // Задача №2
-function Hello(name, lastname, age) {
+// function Hello(name, lastname, age) {
 
-    if (typeof Number(name) === "string") {        
-        console.log("Вы не ввели именя ");
-    } else {
-        if (age < 5) {
-            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
-        } else {
-            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
+//     if (typeof Number(name) === "string") {        
+//         console.log("Вы не ввели именя ");
+//     } else {
+//         if (age < 5) {
+//             console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
+//         } else {
+//             console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
 
-        }
+//         }
+//     }
+//     if (!(typeof Number(lastname) === "string")) {
+//         console.log("Вы ввели число вместо фамилии ");
+//     } else {
+//         if (age < 5) {
+//             console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
+//         } else {
+//             console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
+
+//         }
+//     }
+// }
+// Hello("7", "Петров", "3")
+
+
+// Задача №3
+
+function Gender(m, f) {
+    if (m === "M" && f === "F") {
+        console.log("Ваш пол мужской " + "," + " " + "Ваш пол женский ")
     }
-    if (!(typeof Number(lastname) === "string")) {
-        console.log("Вы ввели число вместо фамилии ");
-    } else {
-        if (age < 5) {
-            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
-        } else {
-            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
-
-        }
+    else {
+        if (m !== "M" && f === "F")
+            console.log("Ваш пол не определен  " + "," + " " + "Ваш пол женский ")
+        if (m === "M" && f !== "F")
+            console.log("Ваш пол мужской " + "," + " " + "Ваш пол не определен  ")
     }
-
-
-
 }
-Hello("Иван", "7", "6")
+Gender("M", "F")
