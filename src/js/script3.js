887e070cbb0082e644777db44b3bacad0b9d482a
Russs11 <russs.khalilov@gmail.com>
2022-06-09 14:39:15 +0300

    tasks_09.06.2022

--- src/js/script3.js
+++ src/js/script3.js
@@ -9,9 +9,42 @@
 // }
 // squareItUp(333)
 
-function multiplication(a,b) {
 
-    s = a * b;
-    console.log(s);
+
+
+// Задача №1
+// function multiplication(a,b) {
+//     s = a * b;
+//     console.log(s);
+// }
+// multiplication(78, 38)
+
+// Задача №2
+function Hello(name, lastname, age) {
+
+    if (!(typeof Number(name) === "string" )) {
+        
+        console.log("Вы ввели число вместо имени ");
+    } else {
+        if (age < 5) {
+            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
+        } else {
+            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
+
+        }
+    }
+    if (!(typeof Number(lastname) === "string")) {
+        console.log("Вы ввели число вместо фамилии ");
+    } else {
+        if (age < 5) {
+            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " года")
+        } else {
+            console.log("Привет " + name + " " + lastname + " с возрастом " + age + " лет")
+
+        }
+    }
+
+
+
 }
-multiplication(78,38)
+Hello("Иван", "7", "6")
