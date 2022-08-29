2575b6f5b7c8ace9e0cd1b384244e83c308b987f
Russs11 <russs.khalilov@gmail.com>
2022-08-29 15:23:26 +0300

    code_wars_29.08.2022

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -140,18 +140,38 @@
 // SnailOnTree()
 
 
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth() + 1;
-let yyyy = today.getFullYear();
-if (dd < 10) {
-    dd = '0' + dd;
+// let today = new Date();
+// let dd = today.getDate();
+// let mm = today.getMonth() + 1;
+// let yyyy = today.getFullYear();
+// if (dd < 10) {
+//     dd = '0' + dd;
+// }
+// if (mm < 10) {
+//     mm = '0' + mm;
+// }
+// let day = dd + '.' + mm + '.' + yyyy;
+// function Whatday() {
+//     console.log(day);
+// }
+// Whatday()
+
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example(Input-- > Output):
+
+// 39 -- > 3(because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit)
+// 999 -- > 4(because 9 * 9 * 9 = 729, 7 * 2 * 9 = 126, 1 * 2 * 6 = 12, and finally 1 * 2 = 2)
+// 4 -- > 0(because 4 is already a one - digit number)
+function persistence(num) {
+    let numArr = []
+    numArr = ('' + num).split('')
+    console.log(numArr.map(Number))
+    let result = 1
+    for (let i = 0; i < numArr.length; i++) {
+        result *= numArr[i]
+    }
+    console.log(result);
+
 }
-if (mm < 10) {
-    mm = '0' + mm;
-}
-let day = dd + '.' + mm + '.' + yyyy;
-function Whatday() {
-    console.log(day);
-}
-Whatday()
+persistence(999)
