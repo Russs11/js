c8d4cea183f408aeb3789f31eac6c556cce874a9
Russs11 <russs.khalilov@gmail.com>
2022-08-31 17:34:37 +0300

    code_wars_31.08.2022

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -170,25 +170,48 @@
 
 // 39 -- > 3(because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit)
 // 999 -- > 4(because 9 * 9 * 9 = 729, 7 * 2 * 9 = 126, 1 * 2 * 6 = 12, and finally 1 * 2 = 2)
-// 4 -- > 0(because 4 is already a one - digit number)
-function persistence(num) {
-    let numArr = []
-    numArr = ('' + num).split('')
-    let iterationNum = 0
-    function counter(arr) {
-    let result = 1
-        arr.forEach(item => {
-            result *= item
-        })    
-        result = ('' + result).split('')
-        return result
+// // 4 -- > 0(because 4 is already a one - digit number)
+// function persistence(num) {
+//     let numArr = []
+//     numArr = ('' + num).split('')
+//     let iterationNum = 0
+//     function counter(arr) {
+//     let result = 1
+//         arr.forEach(item => {
+//             result *= item
+//         })
+//         result = ('' + result).split('')
+//         return result
+// }
+
+//     while (numArr.length > 1) {
+//         iterationNum++
+//         numArr = counter(numArr)
+//     }
+//     console.log(iterationNum);
+// }
+
+// persistence(999);
+
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+//     Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers) {
+    const newNumbers = numbers.replace(/\s+/g, m => (m == ' ') ? ',' : '');
+
+
+    console.log(newNumbers)
+    // numbers.split('')
+    // console.log(Math.min(numbers))  
 }
 
-    while (numArr.length > 1) {
-        iterationNum++
-        numArr = counter(numArr)
-    }
-    console.log(iterationNum);
-}
-
-persistence(999);
+highAndLow("1 2 3 4 5")
